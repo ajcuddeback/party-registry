1daf6d2fb08a0f81f50f3173b29f44c6aaecd3a9
ajcudd <ajcuddeback@gmail.com>
2021-04-25 20:28:06 -0400

    added create user and get user controllers

---
+++ server/controllers/user-controllers.js
@@ -0,0 +1,30 @@
+const { User } = require('../models');
+
+const { signToken } = require('../utils/auth');
+
+module.exports = {
+    // get a single user by either their id or their username
+    async getSingleUser({ user = null, params }, res) {
+        const userData = await User.findOne({
+            $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+        });
+
+        if (!userData) {
+            return res.status(400).json({ message: 'Cannot find a user with this id!' })
+        }
+
+        res.json(userData);
+    },
+
+    async createUser({ body }, res) {
+        const userData = await User.create(body);
+
+        if(!userData) {
+            return res.status(400).json({ message: 'Something went wrong!' });
+        };
+
+        const token = await signToken(userData);
+
+        res.json({ token, userData });
+    }
+}
